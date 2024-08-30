b0910a052365d9d77adf7bae0588240acb1263ab
Arūnas Smaliukas <arunas@smala.lt>
2024-08-30 14:32:35 +0300

    should filter by speciesType

--- services/forms.service.ts
+++ services/forms.service.ts
@@ -4,6 +4,7 @@ import moleculer, { Context, RestSchema } from 'moleculer';
 import { Action, Event, Method, Service } from 'moleculer-decorators';
 
 import DbConnection, { MaterializedView } from '../mixins/database.mixin';
+import { TaxonomySpeciesType } from './taxonomies.species.service';
 
 import PostgisMixin, { areaQuery, distanceQuery } from 'moleculer-postgis';
 
@@ -541,56 +542,29 @@ export interface Form extends BaseModelInterface {
 })
 export default class FormsService extends moleculer.Service {
   @Method
-  async assignTaxonomyFilter(ctx: any) {
+  async speciesTypeFilter(ctx: any) {
     ctx.params.query = parseToObject(ctx.params.query);
 
     ctx.params.query ||= {};
-    //        const queryKeys = Object.keys(taxonomiesToFilter).filter(
-    //          (i: string) => taxonomiesToFilter[i].filter,
-    //        );
-    //
-    //        const hasAnyQuery = queryKeys.some(
-    //          (key) => !!ctx.params.query[taxonomiesToFilter[key].queryKey],
-    //        );
-    //        if (!hasAnyQuery) return ctx;
-    //
-    //        let items = await ctx.call('taxonomies.kingdoms.find', {
-    //          populate: 'phylums',
-    //        });
-    //
-    //        const reduceItems = (items: any[], key: string) => {
-    //          return items.reduce((acc: any[], i) => {
-    //            return [...acc, ...i[key]];
-    //          }, []);
-    //        };
-    //
-    //        const filterItems = (items: any[], id?: number) => {
-    //          if (!id) return items;
-    //          return items.filter((i) => i.id == id);
-    //        };
-    //
-    //        if (taxonomiesToFilter.KINGDOM.filter) {
-    //          items = filterItems(items, ctx.params.query.kingdom);
-    //          items = reduceItems(items, 'phylums');
-    //        }
-    //
-    //        if (taxonomiesToFilter.PHYLUM.filter) {
-    //          items = filterItems(items, ctx.params.query.phylum);
-    //          items = reduceItems(items, 'classes');
-    //        }
-    //
-    //        if (taxonomiesToFilter.CLASS.filter) {
-    //          items = filterItems(items, ctx.params.query.class);
-    //          items = reduceItems(items, 'species');
-    //        }
-    //
-    //        queryKeys.map((key) => {
-    //          delete ctx.params.query[taxonomiesToFilter[key].queryKey];
-    //        });
-    //
-    //        ctx.params.query.id = {
-    //          $in: items.map((i: any) => i.id),
-    //        };
+
+    if (ctx.params.query.speciesType) {
+      if (TaxonomySpeciesType.hasOwnProperty(ctx.params.query.speciesType)) {
+        const speciesIds = await ctx.call('taxonomies.species.find', {
+          query: {
+            type: ctx.params.query.speciesType,
+          },
+          fields: ['id'],
+        });
+
+        if (speciesIds?.length) {
+          ctx.params.query.species = {
+            $in: speciesIds.map((i: any) => i.id),
+          };
+        }
+      }
+
+      delete ctx.params.query.speciesType;
+    }
 
     return ctx;
   }
