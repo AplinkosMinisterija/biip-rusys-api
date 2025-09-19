b5e811ae86827c20d30a3bcc1891218d5c2b743f
LWangllix <vanglikas.lukas@gmail.com>
2025-09-19 10:14:35 +0300

    cleanup

--- services/maps.service.ts
+++ services/maps.service.ts
@@ -127,7 +127,7 @@ export default class MapsService extends moleculer.Service {
             mapsInvaPlacesIntroducedLayerId,
             mapsInvaNoQuantityIntroducedFormsLayerId,
           ].join(',')
-        : [mapsInvaPlacesInvasiveLayerId].join(','),
+        : mapsInvaPlacesInvasiveLayerId,
     });
   }
 
